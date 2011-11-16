daa9dd35e4d0b9503b0bc3c7ba98e32843bb8db8
chicosoftware <chicosoftware@gmail.com>
2011-11-15 21:45:33 -0500

    v 1.2

--- AiwConector/Form1.vb
+++ AiwConector/Form1.vb
@@ -23,33 +23,31 @@
     Dim proceso As New System.Diagnostics.Process
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        urls = connect & TextBox1.Text
-        savestart = cab1 & cab2 & cab3 & vbNewLine & vbNewLine & cab4 & vbNewLine & cab5 & vbNewLine & cab6 & vbNewLine & _
-        red1 & red2 & red3 & urls & red4 & vbNewLine & cab7 & vbNewLine & cab71 & vbNewLine & cab8 & vbNewLine & cab81 & _
-        vbNewLine & cab10
-        System.IO.File.WriteAllText(Application.StartupPath & "\conector.html", savestart)
-        With (proceso)
-            .StartInfo.FileName = Application.StartupPath & "\conector.html"
-            .Start()
-        End With
-        If CheckBox1.Checked = True Then
+        If TextBox1.Text = "" Then
+            MsgBox("Debe introducir almenos una direccion IP", MsgBoxStyle.Exclamation, "Atencion")
+        Else
+            urls = connect & TextBox1.Text
+            savestart = cab1 & cab2 & cab3 & vbNewLine & vbNewLine & cab4 & vbNewLine & cab5 & vbNewLine & cab6 & vbNewLine & _
+            red1 & red2 & red3 & urls & red4 & vbNewLine & cab7 & vbNewLine & cab71 & vbNewLine & cab8 & vbNewLine & cab81 & _
+            vbNewLine & cab10
+            System.IO.File.WriteAllText(Application.StartupPath & "\conector.html", savestart)
+            With (proceso)
+                .StartInfo.FileName = Application.StartupPath & "\conector.html"
+                .Start()
+            End With
+        End If
 
+        If CheckBox1.Checked = True Then
+            ListBox1.Items.Add(TextBox1.Text.Trim)
         End If
     End Sub
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
         Form2.Show()
-
     End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
         ListBox1.Items.Remove(ListBox1.SelectedItem)
     End Sub
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
-
-   
 
     Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
         TextBox1.Text = ""
